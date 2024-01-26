fb2369ad9d691f5ba63df00f9e440d8c37cc33b8
Irfan Yilmaz <iy.usa@outlook.com>
2024-01-26 13:00:39 -0500

    finish Module model, allocate IList of ContentItem
    create private _name and _description fields
    add seperate public getter/setter for above private fields
    create internal IList holding ContentItem models in Module
    override ToString method for Module

--- EduPlatform/EduPlatform/models/Module.cs
+++ EduPlatform/EduPlatform/models/Module.cs
@@ -3,10 +3,27 @@
 namespace EduPlatform.Models{
 
     public class Module {
-
-        Module() {
-            
+        public Module() {
+            content = new List<ContentItem>();
         }
+
+        private string? _name;
+        public string? Name {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private string? _description;
+        public string? Description {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Description}";
+        }
+        private IList<ContentItem>? content;
 
     }
 
